5111be182996e44372155562a88fc36f6a9d272b
Anjana Hengaju <an.hengaju@gmail.com>
2019-06-29 11:01:05 -0500

    Modal with detail

--- client/src/components/PlantDetail/index.js
+++ client/src/components/PlantDetail/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "../../assets/css/style.css";
 import Modal from "react-modal";
+import { Col, Row, Container } from "../Grid";
+import Jumbotron from "../Jumbotron";
 
 class PlantDetail extends Component {
   state = {
@@ -15,17 +17,18 @@ class PlantDetail extends Component {
   }
   onClick() {
     this.setState({ isOpen: !this.state.isOpen });
-  };
+  }
   displayPlant(props) {
     return (
       <div id="details" className="card border-0 mb-3">
         <div className="row no-gutters">
           <div className="col-md-4">
-            <h3 onClick={this.onClick.bind(this)} >{props.plant.Name}</h3>
+            <h3 onClick={this.onClick.bind(this)}>{props.plant.Name}</h3>
             <img
               alt={props.plant.Name}
               src={props.plant.Image}
               className="card-img"
+              onClick={this.onClick.bind(this)}
             />
           </div>
           <div className="col-md-8">
@@ -70,19 +73,62 @@ class PlantDetail extends Component {
             describedby: "full_description"
           }}
         >
-          <h1 id="heading">Alert</h1>
-          <div id="full_description">
-            <p>ho onodxosisyuctdsvayuchijxok;sjuyxvhsjklxm</p>
-          </div>
-          <button onClick={this.onClick.bind(this)} isOpen={this.state.isOpen}>close</button>
+          <Container fuild>
+            <Row>
+              <Col size="md-6">
+                <h1>{this.props.plant.Name}</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col size="md-6">
+                <ul>
+                  <li>
+                    Image Link: <img src={this.props.plant.Image} />
+                  </li>
+
+                  <li>Type: {this.props.plant.Type}</li>
+                  <li>Name: {this.props.plant.Name}</li>
+                  <li>Botanical Name: {this.props.plant.BotanicalName}</li>
+                  <li>Height: {this.props.plant.Height}</li>
+                  <li>Spacing: {this.props.plant.Spacing}</li>
+                  <li>Plant Spacing: {this.props.plant.PS}</li>
+                  <li>
+                    Row Spacing: <href>{this.props.plant.RS}</href>
+                  </li>
+                  <li>Depth: {this.props.plant.Depth}</li>
+                  <li>Spread: {this.props.plant.Spread}</li>
+                  <li>Sun Light: {this.props.plant.Light}</li>
+                  <li>Yield: {this.props.plant.Yield}</li>
+                </ul>
+              </Col>
+              <Col size="md-6">
+                <ul>
+                  <li>
+                    Foliage: <href>{this.props.plant.Foliage}</href>
+                  </li>
+                  <li>Growth: {this.props.plant.Growth}</li>
+                  <li>Fruit: {this.props.plant.Fruit}</li>
+                  <li>Size: {this.props.plant.Size}</li>
+                  <li>Maturity: {this.props.plant.Maturity}</li>
+                  <li>Zone: {this.props.plant.Zone}</li>
+                  <li>Germination: {this.props.plant.Germination}</li>
+                  <li>Form: {this.props.plant.Form}</li>
+                  <li>Flowers: {this.props.plant.Flowers}</li>
+                  <li>Soil: {this.props.plant.Soil}</li>
+                  <li>Seeds: {this.props.plant.Seeds}</li>
+                  <li>Pruning: {this.props.plant.Pruning}</li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+
+          <button onClick={this.onClick.bind(this)} isOpen={this.state.isOpen}>
+            close
+          </button>
         </Modal>
       </div>
     );
   }
 }
-
-
-
-
 
 export default PlantDetail;
