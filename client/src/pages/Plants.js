e601d9106ab8a998f3707293d7bc7cb6ac845d77
Anjana Hengaju <an.hengaju@gmail.com>
2019-07-06 17:40:49 -0500

    save layout button save the json with name, dimesionas and layout of garden

--- client/src/pages/Plants.js
+++ client/src/pages/Plants.js
@@ -28,7 +28,10 @@ class Plants extends Component {
     gardenWeight: 12,
     seedSpacing: 5,
     bgColor: "",
-    defaultLayout: []
+    defaultLayout: [],
+    name: "",
+    length: "",
+    breadth: ""
   };
 
   componentDidMount() {
@@ -62,13 +65,52 @@ class Plants extends Component {
       [name]: value
     });
   };
-  
-  getRandomColor(){
-    let colorValues = ["#F2506E", "#07418C", "#03738C", "#91E0F2", "#F2CA52", "#077336", "#145932", "#F27405", "#D9411E", "#A6A486", "#F2D5C4", "#8C2E6B", "#468C8C", "#F2DF80", "#BFB4AA", "#F27777" ];
+
+  getRandomColor() {
+    let colorValues = [
+      "#F2506E",
+      "#07418C",
+      "#03738C",
+      "#91E0F2",
+      "#F2CA52",
+      "#077336",
+      "#145932",
+      "#F27405",
+      "#D9411E",
+      "#A6A486",
+      "#F2D5C4",
+      "#8C2E6B",
+      "#468C8C",
+      "#F2DF80",
+      "#BFB4AA",
+      "#F27777"
+    ];
     return colorValues[Math.floor(Math.random() * colorValues.length)];
   }
 
- 
+  updateName = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  updateLength = event => {
+    this.setState({ length: event.target.value });
+  };
+
+  updateBreadth = event => {
+    this.setState({ breadth: event.target.value });
+  };
+
+  handleSubmit(event) {
+    console.log(event.target);
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data);
+    // fetch('/api/form-submit-url', {
+    //   method: 'POST',
+    //   body: data,
+    // });
+  }
+
   handleFormSubmit = event => {
     event.preventDefault();
     let finalPlants = [...this.state.finalPlants];
@@ -86,23 +128,29 @@ class Plants extends Component {
     this.setState({
       plant: null
     });
-    return(finalPlants[finalPlants.length -1].background)
+    return finalPlants[finalPlants.length - 1].background;
   };
 
+  handleGardenSave = newLayout => {
+    const garden = {
+      name: this.state.name,
+      length: this.state.length,
+      breadth: this.state.breadth,
+      layout: newLayout
+    };
+    this.setState({ layout: newLayout });
+    console.log({ newLayout });
+    console.log({ garden });
+  };
 
-  handleGardenSave = (newLayout) => {
-    this.setState({ layout : newLayout });
-  }
-
-  triggerChildAddItem = (bgColor) => {
+  triggerChildAddItem = bgColor => {
     this.refs.addItem.onAddItem(bgColor);
+  };
 
-  }
-
-  addPlantToList = (event) =>{
-   let bgColor =  this.handleFormSubmit(event);
-  this.triggerChildAddItem(bgColor);
-  }
+  addPlantToList = event => {
+    let bgColor = this.handleFormSubmit(event);
+    this.triggerChildAddItem(bgColor);
+  };
 
   render() {
     return (
@@ -110,6 +158,46 @@ class Plants extends Component {
         <Nav2 />
         <Row>
           <Col size="sm-4">
+            <div className="form-group">
+              <label for="garden-name">
+                Name your garden and specify dimensions
+              </label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.updateName}
+              />
+            </div>
+            <div className="row">
+              <div className="dimension-1 col">
+                <input
+                  type="text"
+                  name="length"
+                  className="form-control"
+                  placeholder="feet"
+                  value={this.state.length}
+                  onChange={this.updateLength}
+                />
+              </div>
+
+              <div className="dimension-2 col">
+                <h6> by </h6>
+              </div>
+
+              <div className="dimension-3 col">
+                <input
+                  type="text"
+                  name="breadth"
+                  className="form-control w-200"
+                  placeholder="feet"
+                  value={this.state.breadth}
+                  onChange={this.updateBreadth}
+                />
+              </div>
+            </div>
             <SearchBar>
               <AsyncTypeahead
                 ref={typeahead => (this.typeahead = typeahead)}
@@ -134,9 +222,9 @@ class Plants extends Component {
               />
             </SearchBar>
 
-
-            <FormBtn onClick={this.addPlantToList} disabled={!this.state.plant} >Add plant</FormBtn>
-
+            <FormBtn onClick={this.addPlantToList} disabled={!this.state.plant}>
+              Add plant
+            </FormBtn>
           </Col>
           <Col size="sm-8">
             {this.state.plant && (
@@ -149,19 +237,25 @@ class Plants extends Component {
               <div>
                 <ul>
                   {this.state.finalPlants.map(plant => (
-                     <Col size="sm-6 col-md-6 col-lg-4">
-                     <div className="listed-plant" >
-                       <li className="list-group-item" style={{backgroundColor: plant.background}}>{plant.name}
-                       <NumberBadge 
-                        id={plant.id}
-                        key={plant.key}
-                        name={plant.name}
-                        // style={{backgroundColor: plant.background}}
-                        />
-                       <DeleteBtn onClick={() => this.removePlant(plant.id)} />
-                       </li>
-                     </div>
-                   </Col>
+                    <Col size="sm-6 col-md-6 col-lg-4">
+                      <div className="listed-plant">
+                        <li
+                          className="list-group-item"
+                          style={{ backgroundColor: plant.background }}
+                        >
+                          {plant.name}
+                          <NumberBadge
+                            id={plant.id}
+                            key={plant.key}
+                            name={plant.name}
+                            // style={{backgroundColor: plant.background}}
+                          />
+                          <DeleteBtn
+                            onClick={() => this.removePlant(plant.id)}
+                          />
+                        </li>
+                      </div>
+                    </Col>
                   ))}
                 </ul>
               </div>
@@ -172,16 +266,16 @@ class Plants extends Component {
         <Row>
           <Col size="sm-12">
             <LocalStorageOriginal
-            // cols={10}
-            // rowHeight={30}
-            gardenWidth={this.state.gardenWeight}
-            ref="addItem"
-            seedSpacing={this.state.seedSpacing}
-            defaultLayout={this.state.defaultLayout}
-            handleGardenSave={this.handleGardenSave}
-            finalPlants={this.state.finalPlants}
-            plant={this.state.plant}
-            bgColor={this.state.bgColor}
+              // cols={10}
+              // rowHeight={30}
+              gardenWidth={this.state.gardenWeight}
+              ref="addItem"
+              seedSpacing={this.state.seedSpacing}
+              defaultLayout={this.state.defaultLayout}
+              handleGardenSave={this.handleGardenSave}
+              finalPlants={this.state.finalPlants}
+              plant={this.state.plant}
+              bgColor={this.state.bgColor}
             />
           </Col>
         </Row>
