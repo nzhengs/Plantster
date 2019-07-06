c2b52936d95de08d7153224f63850d1168912dc0
Eva Anderson <eva.k.anderson@gmail.com>
2019-07-06 10:32:56 -0500

    reverted to children vs. props

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 // This file exports both the List and ListItem components
 
-export function List({ props }) {
+export function List({ children }) {
   return (
     <div className="list-overflow-container col-sm-4 col-md-4 col-lg-8">
       <ul className="list-group">{children}</ul>
