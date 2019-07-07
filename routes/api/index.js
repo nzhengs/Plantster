4e56f4bf5373e32c2cef1210744eb860b768b30a
Anjana Hengaju <an.hengaju@gmail.com>
2019-07-06 23:36:54 -0500

    remove duplicate route, conflict fix

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,13 @@
 const router = require("express").Router();
 const plantRoutes = require("./plants");
+const user = require("./user");
+const gardenRoutes = require("./gardens");
 
 const userRoutes = require("./users");
 // plants routes
 router.use("/plants", plantRoutes);
-router.use("/users", userRoutes );
-const user = require("./user");
-const gardenRoutes = require("./gardens");
-// plants routes
-router.use("/plants", plantRoutes);
+router.use("/users", userRoutes);
 router.use("/gardens", gardenRoutes);
-
 router.use("/user", user);
 
 module.exports = router;
